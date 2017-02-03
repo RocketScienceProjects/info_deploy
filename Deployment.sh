4f4262158697780a08e6bc0142416e7603468560
okram999 <nirish.okram@gmail.com>
2017-02-03 16:57:15 -0600

    cleansing the code

--- Deployment.sh
+++ Deployment.sh
@@ -5,13 +5,8 @@ source ./environmental_variables.env
 
 #Defining VARIABLES to be used in the script
 InfaMigPath=$(pwd)
-# echo "$InfaMigPath"
-# LogFileDir=$InfaMigPath/Logs
-# echo "$LogFileDir"
-# current_date=`date +'%Y-%m-%d %H%M%S'`
-# echo "$current_date"
-# LogFileName=Infa_DG_LB_$current_date.log
-# echo "$LogFileName"
+echo "$InfaMigPath"
+
 
 #Setting the parameters passed in the script as env variables for the session
 export USERNAME=$1
@@ -21,27 +16,20 @@ export TGT_REP=$4
 export NAME=$5
 export ACTION=$6
 
-##### Nullifying the log file
-# mkdir $LogFileDir
-# touch $LogFileDir/$LogFileName
-#cat /dev/null>$LogFileDir/$LogFileName
 
-##### Connecting to the Source repository
-##### $DOMAIN defined in the env file
+##### Connecting to the Source repository, $DOMAIN defined in the env file
 echo "Connecting to the Repository "$SRC_REP
 pmrep connect -r $SRC_REP -d $DOMAIN -n $USERNAME -x $PASSWORD -s $USERSECURITYDOMAIN #2>>$LogFileDir/$LogFileName 1>>$LogFileDir/$LogFileName
 RETURN_CODE=$?
-echo "RETURN_CODE: "$RETURN_CODE  #>>$LogFileDir/$LogFileName
+echo "RETURN_CODE: "$RETURN_CODE
 
 	if [ $RETURN_CODE == 0 ]
 	then
 	echo "Connected to the Repository "$SRC_REP
 	echo
-	echo "Connected to the Repository "$SRC_REP #>>$LogFileDir/$LogFileName
+	echo "Connected to the Repository "$SRC_REP
 	else
 	echo "Failed to Connect to the Repository "$SRC_REP
-	# echo "Failed to Connect to the Repository "$SRC_REP #>>$LogFileDir/$LogFileName
-	# echo "Check the log file "$LogFileDir/$LogFileName
 	echo
 	exit 1
 	fi
@@ -50,19 +38,18 @@ echo "RETURN_CODE: "$RETURN_CODE  #>>$LogFileDir/$LogFileName
 if [ "$ACTION" == DG_DELETE ]
 then
 echo "Deleting the Deployment Group "$NAME
-pmrep deletedeploymentgroup -p $NAME -f #2>>$LogFileDir/$LogFileName 1>>$LogFileDir/$LogFileName
+pmrep deletedeploymentgroup -p $NAME -f
 RETURN_CODE=$?
-echo "RETURN_CODE: "$RETURN_CODE  #>>$LogFileDir/$LogFileName
+echo "RETURN_CODE: "$RETURN_CODE
 
 	if [ $RETURN_CODE == 0 ]
 	then
 	echo "Deleted the Deployment Group "$NAME
 	echo
-	echo "Deleted the Deployment Group "$NAME #>>$LogFileDir/$LogFileName
+	echo "Deleted the Deployment Group "$NAME
 	else
 	echo "Deployment Group "$NAME " is not present / invalid credentials."
-	echo "Deployment Group "$NAME " is not present." #>>$LogFileDir/$LogFileName
-	# echo "Check the log file "$LogFileDir/$LogFileName
+	echo "Deployment Group "$NAME " is not present."
 	echo
 	exit 1
 	fi
@@ -73,18 +60,18 @@ fi
 if [ "$ACTION" == DG_CLEAR ]
 then
 
-pmrep cleardeploymentgroup -p $NAME -f #2>>$LogFileDir/$LogFileName 1>>$LogFileDir/$LogFileName
+pmrep cleardeploymentgroup -p $NAME -f
 RETURN_CODE=$?
-echo "RETURN_CODE: "$RETURN_CODE  #>>$LogFileDir/$LogFileName
+echo "RETURN_CODE: "$RETURN_CODE
 
 	if [ $RETURN_CODE == 0 ]
 	then
 	echo "Cleared the Deployment Group "$NAME
 	echo
-	echo "Cleared the Deployment Group "$NAME #>>$LogFileDir/$LogFileName
+	echo "Cleared the Deployment Group "$NAME
 	else
 	echo "Deployment Group "$NAME " is not present / invalid credentials."
-	echo "Deployment Group "$NAME " is not present." #>>$LogFileDir/$LogFileName
+	echo "Deployment Group "$NAME " is not present."
 	echo "Check the log file "$LogFileDir/$LogFileName
 	echo
 	exit 1
@@ -97,19 +84,16 @@ fi
 if [ "$ACTION" == DG_CREATE -o "$ACTION" == DG_C_A_D ]
 then
 
-pmrep createdeploymentgroup -p $NAME -t static -q DUMMY -u shared #2>>$LogFileDir/$LogFileName 1>>$LogFileDir/$LogFileName
+pmrep createdeploymentgroup -p $NAME -t static -q DUMMY -u shared
 RETURN_CODE=$?
-echo "RETURN_CODE: "$RETURN_CODE  #>>$LogFileDir/$LogFileName
+echo "RETURN_CODE: "$RETURN_CODE
 
 	if [ $RETURN_CODE == 0 ]
 	then
 	echo "Created the Deployment Group "$NAME
 	echo
-	#echo "Created the Deployment Group "$NAME >>$LogFileDir/$LogFileName
 	else
 	echo "Deployment Group "$NAME " is already available / invalid credentials."
-	#echo "Deployment Group "$NAME " is already available / invalid credentials." #>>$LogFileDir/$LogFileName
-	#echo "Check the log file "$LogFileDir/$LogFileName
 	echo
 	exit 1
 	fi
@@ -131,17 +115,15 @@ set -- $var
 GRP_NM=$1
 ACCESS=$2
 
-pmrep AssignPermission -o deploymentgroup -n $NAME -g $GRP_NM -s $USERSECURITYDOMAIN -p $ACCESS #2>>$LogFileDir/$LogFileName 1>>$LogFileDir/$LogFileName
+pmrep AssignPermission -o deploymentgroup -n $NAME -g $GRP_NM -s $USERSECURITYDOMAIN -p $ACCESS
 RETURN_CODE=$?
-echo "RETURN_CODE: "$RETURN_CODE  #>>$LogFileDir/$LogFileName
+echo "RETURN_CODE: "$RETURN_CODE
 
 	if [ $RETURN_CODE == 0 ]
 	then
 	echo $GRP_NM " - " $ACCESS " permission is given to the Deployment Group "$NAME
-	#echo $GRP_NM " - " $ACCESS " permission is given to the Deployment Group "$NAME #>>$LogFileDir/$LogFileName
 	else
 	echo "Informatica Group "$GRP_NM " is not available / invalid credentials."
-	# echo "Informatica Group "$GRP_NM " is not available / invalid credentials." #>>$LogFileDir/$LogFileName
 	echo "Check the log file "$LogFileDir/$LogFileName
 	echo
 	exit 1
@@ -152,7 +134,6 @@ fi
 
 fi
 
-##### Add Informatica Objects to the Deployment group.
 if [ "$ACTION" == DG_ADD -o "$ACTION" == DG_C_A_D ]
 then
 
@@ -168,27 +149,22 @@ FLDR_NM=$2
 OBJ_TYPE=$3
 OBJ_NM=$4
 
-	##### Checking the connected repository and repository name in the inventory list.
 	if [ "$REPO_NM" != "$SRC_REP" ]
 	then
 	echo "Connected repository "$SRC_REP" is not equal to the repository name in file "$REPO_NM
-	# echo "Connected repository "$SRC_REP" is not equal to the repository name in file "$REPO_NM #>>$LogFileDir/$LogFileName
 	echo
 	exit 1
 	fi
 
-pmrep addtodeploymentgroup -p $NAME -n $OBJ_NM -o $OBJ_TYPE -f $FLDR_NM -d all #2>>$LogFileDir/$LogFileName 1>>$LogFileDir/$LogFileName
+pmrep addtodeploymentgroup -p $NAME -n $OBJ_NM -o $OBJ_TYPE -f $FLDR_NM -d all
 RETURN_CODE=$?
-echo "RETURN_CODE: "$RETURN_CODE #>>$LogFileDir/$LogFileName
+echo "RETURN_CODE: "$RETURN_CODE
 
 	if [ $RETURN_CODE == 0 ]
 	then
 	echo "Added "$OBJ_NM " to the Deployment Group "$NAME
-	# echo "Added "$OBJ_NM " to the Deployment Group "$NAME #>>$LogFileDir/$LogFileName
 	else
 	echo "Object name "$OBJ_NM" is not available / invalid credentials."
-	# echo "Object name "$OBJ_NM" is not available / invalid credentials." #>>$LogFileDir/$LogFileName
-	# echo "Check the log file "$LogFileDir/$LogFileName
 	echo
 	exit 1
 	fi
@@ -210,19 +186,14 @@ echo "Starting Deployment of "$NAME" to target Repository "$TGT_REP.
 date
 pmrep deploydeploymentgroup -p $NAME -c $InfaMigPath/DeployOptions.xml -r $TGT_REP -n $USERNAME -s $USERSECURITYDOMAIN -x $PASSWORD -l $LogFileDir/$NAME.log 2>>$LogFileDir/$LogFileName 1>>$LogFileDir/$LogFileName
 RETURN_CODE=$?
-echo "RETURN_CODE: "$RETURN_CODE  #>>$LogFileDir/$LogFileName
-
+echo "RETURN_CODE: "$RETURN_CODE
 	if [ $RETURN_CODE == 0 ]
 	then
 	echo "Deployment of "$NAME" to target Repository "$TGT_REP" was successful."
 	echo
-	# echo "Deployment of "$NAME" to target Repository "$TGT_REP" was successful." #>>$LogFileDir/$LogFileName
 	date
 	else
 	echo "Deployment of "$NAME" failed."
-	# echo "Deployment of "$NAME" failed." #>>$LogFileDir/$LogFileName
-	# echo "Check the log file "$LogFileDir/$LogFileName
-	# echo "Check the deployment log file "$LogFileDir/$NAME.log
 	echo
 	exit 1
 	fi
@@ -233,9 +204,9 @@ fi
 if [ "$ACTION" == LB_CREATE -o "$ACTION" == LB_C_A ]
 then
 
-pmrep createlabel -a $NAME #2>>$LogFileDir/$LogFileName 1>>$LogFileDir/$LogFileName
+pmrep createlabel -a $NAME
 RETURN_CODE=$?
-echo "RETURN_CODE: "$RETURN_CODE  #>>$LogFileDir/$LogFileName
+echo "RETURN_CODE: "$RETURN_CODE
 
 	if [ $RETURN_CODE == 0 ]
 	then
@@ -244,7 +215,6 @@ echo "RETURN_CODE: "$RETURN_CODE  #>>$LogFileDir/$LogFileName
 	echo "Created the Label "$NAME >>$LogFileDir/$LogFileName
 	else
 	echo "Label "$NAME " is already available / invalid credentials."
-	# echo "Label "$NAME " is already available / invalid credentials." #>>$LogFileDir/$LogFileName
 	echo "Check the log file "$LogFileDir/$LogFileName
 	echo
 	exit 1
@@ -267,17 +237,15 @@ set -- $var
 GRP_NM=$1
 ACCESS=$2
 
-pmrep AssignPermission -o label -n $NAME -g $GRP_NM -s $USERSECURITYDOMAIN -p $ACCESS #2>>$LogFileDir/$LogFileName 1>>$LogFileDir/$LogFileName
+pmrep AssignPermission -o label -n $NAME -g $GRP_NM -s $USERSECURITYDOMAIN -p $ACCESS
 RETURN_CODE=$?
-echo "RETURN_CODE: "$RETURN_CODE  #>>$LogFileDir/$LogFileName
+echo "RETURN_CODE: "$RETURN_CODE
 
 	if [ $RETURN_CODE == 0 ]
 	then
 	echo $GRP_NM " - " $ACCESS " permission is given to the Label "$NAME
-	# echo $GRP_NM " - " $ACCESS " permission is given to the Label  "$NAME #>>$LogFileDir/$LogFileName
 	else
 	echo "Informatica Group "$GRP_NM " is not available / invalid credentials."
-	# echo "Informatica Group "$GRP_NM " is not available / invalid credentials." #>>$LogFileDir/$LogFileName
 	echo "Check the log file "$LogFileDir/$LogFileName
 	echo
 	exit 1
@@ -293,18 +261,17 @@ fi
 if [ "$ACTION" == LB_DELETE ]
 then
 
-pmrep deletelabel -a $NAME -f #2>>$LogFileDir/$LogFileName 1>>$LogFileDir/$LogFileName
+pmrep deletelabel -a $NAME -f
 RETURN_CODE=$?
-echo "RETURN_CODE: "$RETURN_CODE  #>>$LogFileDir/$LogFileName
+echo "RETURN_CODE: "$RETURN_CODE
 
 	if [ $RETURN_CODE == 0 ]
 	then
 	echo "Deleted the Label "$NAME
 	echo
-	echo "Deleted the Label "$NAME #>>$LogFileDir/$LogFileName
+	echo "Deleted the Label "$NAME
 	else
 	echo "Label "$NAME " is not available / invalid credentials."
-	# echo "Label "$NAME " is not available / invalid credentials." #>>$LogFileDir/$LogFileName
 	echo "Check the log file "$LogFileDir/$LogFileName
 	echo
 	exit 1
@@ -332,23 +299,20 @@ OBJ_NM=$4
 	if [ "$REPO_NM" != "$SRC_REP" ]
 	then
 	echo "Connected repository "$SRC_REP" is not equal to the repository name in file "$REPO_NM
-	# echo "Connected repository "$SRC_REP" is not equal to the repository name in file "$REPO_NM #>>$LogFileDir/$LogFileName
 	echo
 	exit 1
 	fi
 
-pmrep applylabel -a $NAME -n $OBJ_NM -o $OBJ_TYPE -f $FLDR_NM -p children #2>>$LogFileDir/$LogFileName 1>>$LogFileDir/$LogFileName
+pmrep applylabel -a $NAME -n $OBJ_NM -o $OBJ_TYPE -f $FLDR_NM -p children
 RETURN_CODE=$?
-echo "RETURN_CODE: "$RETURN_CODE #>>$LogFileDir/$LogFileName
+echo "RETURN_CODE: "$RETURN_CODE
 
 
 	if [ $RETURN_CODE == 0 ]
 	then
 	echo "Applied label "$NAME " to the Infa Object "$OBJ_NM
-	# echo "Applied label "$NAME " to the Infa Object "$OBJ_NM #>>$LogFileDir/$LogFileName
 	else
 	echo "Object name "$OBJ_NM" is not available / invalid credentials."
-	# echo "Object name "$OBJ_NM" is not available / invalid credentials." #>>$LogFileDir/$LogFileName
 	echo "Check the log file "$LogFileDir/$LogFileName
 	echo
 	exit 1
